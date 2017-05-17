eb0ee2311e915d352789d0cff6eb2abd9ca0a8c0
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-05-17 18:00:33 -0400

    character films responder v2 (Garren/Amanda)

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,6 +2,31 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+class Character
+  attr_reader :name, :films, :films_URLs, :film_titles
+
+  def initialize(individual_character)
+    @name = individual_character["name"]
+    @film_titles = []
+    @films_URLs = individual_character["films"]
+  end
+
+  def get_film_titles
+    @films_URLs.each do |url|
+      film_info = RestClient.get(url)
+      film_hash = JSON.parse(film_info)
+      @film_titles << film_hash["title"]
+    end
+  end
+
+  def show_character_movies
+    films_output = @film_titles.join(", ")
+    character_name_output = @name.split.map(&:capitalize).join(' ')
+    puts "#{character_name_output} was in #{films_output}"
+  end
+
+end
+
 def get_character_movies_from_api(character)
   #make the web request
   film_urls_array = []
@@ -10,49 +35,13 @@ def get_character_movies_from_api(character)
   character_hash["results"].each do |individual_character|
     character_index = character_hash["results"].index(individual_character)
       if character_hash["results"][character_index]["name"].downcase == character
-        film_urls_array = character_hash["results"][character_index]["films"]
+        character = Character.new(individual_character)
       end
-      film_urls_array
   end
-
-  films_hash = []
-
-  film_urls_array.each do |url|
-    film_info = RestClient.get(url)
-    film_hash = JSON.parse(film_info)
-    films_hash << film_hash
-  end
-  #binding.pry
-
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
-  films_hash
+  character.get_film_titles
+  character.show_character_movies
 end
 
-def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
-  film_list = []
-  films_hash.each do |film_hash|
-    film_list << "'#{film_hash["title"]}'"
-  end
-
-  film_list.join(", ")
-end
-
-def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  films_output = parse_character_movies(films_hash)
-  character_name_output = character.split.map(&:capitalize).join(' ')
-  puts "#{character_name_output} was in #{films_output}"
-
-end
 
 ## BONUS
 
